8483ac7e08bb6e1731109e995a2a6ca28f1e18e0
Hunter Orges <hunterorges@gmail.com>
2019-03-18 13:57:08 -0600

    Add delete functionality to blog items

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,15 +1,25 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-import { 
-  faTrash, 
-  faSignOutAlt, 
-  faEdit, 
-  faSpinner, 
-  faPlusCircle  
-} from '@fortawesome/free-solid-svg-icons';
+import {
+  faTrash,
+  faSignOutAlt,
+  faEdit,
+  faSpinner,
+  faPlusCircle,
+  faEnvelope,
+  faMapMarkedAlt
+} from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-  return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
-}
+  return library.add(
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusCircle,
+    faEnvelope,
+    faMapMarkedAlt
+  );
+};
 
 export default Icons;
