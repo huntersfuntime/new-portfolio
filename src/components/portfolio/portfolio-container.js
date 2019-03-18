90c7ffc962d84cb9324c9a7044a6ae816a91aa8f
Hunter Orges <hunterorges@gmail.com>
2019-03-18 12:59:30 -0600

    Fix filtering bug and refactor to make it dynamic

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,70 +1,91 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       isLoading: false,
       data: []
-    }
-
+    };
   }
 
-  getPortfolioItems() {
-    axios.get('https://huntersfuntime.devcamp.space/portfolio/portfolio_items')
-    .then(response => {
-      this.setState({
-        data: response.data.portfolio_items
+  getPortfolioItems(filter = null) {
+    axios
+      .get("https://huntersfuntime.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter(item => {
+              return item.category === filter;
+            })
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items
+          });
+        }
       })
-    })
-    .catch(error => {
-      console.log(error);
-    });
+
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return (
-      <PortfolioItem
-        key={item.id} 
-        item={item}
-        />
-      )
-    })
+      return <PortfolioItem key={item.id} item={item} />;
+    });
   }
 
-  handleFilter = (filter) => {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.category === filter;
-      })
-    })
+  handleFilter(filter) {
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.getPortfolioItems();
   }
 
   render() {
     if (this.state.isLoading) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
     return (
-      <div className='portfolio-items-wrapper'>
-        <button className='btn' onClick={() => this.handleFilter('Higher Education')}>
-          Higher Education
-        </button>
-        <button className='btn' onClick={() => this.handleFilter('Influencer')}>
-          Influencer
-        </button>
-        <button className='btn' onClick={() => this.handleFilter('Technology')}>
-          Technology
-        </button>
-        {this.portfolioItems()}
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Higher Education")}
+          >
+            Higher Education
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Influencer")}
+          >
+            Influencer
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Technology")}
+          >
+            Technology
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
       </div>
-    )
+    );
   }
 }
